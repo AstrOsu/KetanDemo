547cd37b960b2359fc180adf0eaded46a46f3081
Luddles <30kmanek@stratfordschools.net>
2022-08-28 09:10:38 -0700

    added file input output to RSA keys

--- Sockets/client.py
+++ Sockets/client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import sys
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import binascii
@@ -7,11 +7,38 @@ import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
-keyPair = RSA.generate(3072)
+
+from os.path import exists
+
+def git_keys():
+    path_to_file = input()    
+    file_exists = exists(path_to_file)
+    if(file_exists):
+        print ("Charizard_exists")
+        keyFile = open("../ssh/clientpubKey", "br")
+        pubKeyPEM= keyFile.read()
+        keyFile.close()
+        keyFile = open("../ssh/clientprivKey", "br")
+        privKeyPEM= keyFile.read()
+        keyFile.close()
+        print (privKeyPEM)
+    else:
+        print ("Charizard is not real")
+        keyPair = RSA.generate(3072)
  
-pubKey = keyPair.publickey() 
-pubKeyPEM = pubKey.exportKey()
-privKeyPEM = keyPair.exportKey()
+        pubKey = keyPair.publickey() 
+        pubKeyPEM = pubKey.exportKey()
+        privKeyPEM = keyPair.exportKey()
+        pubKeyfile = open("../ssh/clientpubKey" , "bw+")
+        pubKeyfile.write(pubKeyPEM)
+        pubKeyfile.close()
+        privKeyfile = open("../ssh/clientprivKey" , "bw+")
+        privKeyfile.write(privKeyPEM)
+        privKeyfile.close()                                                                                                                                                                                                                    
+git_keys()
+exit()
+
+     
 encryptor = PKCS1_OAEP.new(pubKey)
 
 
